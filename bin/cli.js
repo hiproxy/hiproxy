f123fe8a34203835fcc6378c133017ed62ed4235
zdying <zdying@live.com>
2017-04-26 22:46:22 +0800

    add daemon

--- bin/cli.js
+++ bin/cli.js
@@ -38,18 +38,14 @@ _args
 // 解析参数，但是不执行命令
 global.args = _args.parse(false);
 
-function openLog(logfile) {
-    return require('fs').createWriteStream(logfile);
-}
-
-if(global.args.daemon && !process.env.__daemon){
+if (global.args.daemon && !process.env.__daemon) {
   // 如果指定后台运行模块，并且不是child进程，启动child进程
   require('daemon')({
     env: process.env,
     stdout: process.stdout,
     stderr: process.stderr
   });
-}else{
+} else {
   // 没有指定后台运行，或者是child进程
   _args.execute();
 }
