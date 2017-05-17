5b73afb910b448d03b9f8c30867cb4b8d7dbc516
zdying <zdying@live.com>
2017-05-17 18:38:00 +0800

    fixbug

--- bin/cli.js
+++ bin/cli.js
@@ -50,6 +50,8 @@ _args
 // 解析参数，但是不执行命令
 global.args = _args.parse(false);
 
+mkdirp(hiproxyDir);
+
 if (!global.args.__error__) {
   if (global.args.daemon && !process.env.__daemon) {
     // 如果指定后台运行模块，并且不是child进程，启动child进程
