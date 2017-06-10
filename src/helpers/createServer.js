23badcad9b7e6128ce115ea651919898f3babda1
zdying <zdying@live.com>
2017-06-10 19:07:59 +0800

    代码优化

--- src/helpers/createServer.js
+++ src/helpers/createServer.js
@@ -6,6 +6,7 @@ module.exports = {
   _createHTTPServer: function (port, rewrite) {
     return new Promise(function (resolve, reject) {
       var server = require('http').createServer().listen(port);
+      server.keepAliveTimeout = 0;
       server
         .on('listening', function () {
           resolve(server);
@@ -51,7 +52,7 @@ module.exports = {
 
     return new Promise(function (resolve, reject) {
       var server = require('https').createServer(option).listen(port);
-
+      server.keepAliveTimeout = 0;
       server
         .on('listening', function () {
           resolve(server);
