9b9e77fba77684657851cd7c8db998e0384de922
liren.jin <liren.jin@qunar.com>
2017-09-11 10:24:17 +0800

    fix format

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,40 +1,33 @@
 /**
  * @file command `init`
- * @author zdying
+ * @author Vampire-Jason
  */
 
 'use strict';
 
 var fs = require('fs');
-var homedir = require('os-homedir');
 var path = require('path');
 
-
 module.exports = {
-    command: 'init',
-    describe: 'Create a default rewrite file',
-    usage: 'init',
-    fn: function () {
-        //待拷贝的默认rewrite.conf文件位置
-        var filePath = path.join(__dirname, '../../rewrite.conf');
+  command: 'init',
+  describe: 'Create a default rewrite file',
+  usage: 'init',
+  fn: function () {
+    // 待拷贝的默认rewrite.conf文件位置
+    var filePath = path.join(__dirname, '../../rewrite.conf');
 
-        fs.readFile(filePath, 'utf-8', function (err, data) {
-            if (err) {
-                console.log('create a default rewrite file error:', err.message);
-            }
-            else {
-                fs.writeFile(path.resolve(process.cwd(), 'rewrite'), data, function (err) {
-                    if (err) {
-                        console.log('create a default rewrite file error:', err.message);
-
-                    } else {
-                        console.log('Default rewrite file create success.');
-
-                    }
-                });
-            }
+    fs.readFile(filePath, 'utf-8', function (err, data) {
+      if (err) {
+        console.log('Create a default rewrite file error:', err.message);
+      } else {
+        fs.writeFile(path.resolve(process.cwd(), 'rewrite'), data, function (err) {
+          if (err) {
+            console.log('Create a default rewrite file error:', err.message);
+          } else {
+            console.log('Default rewrite file create success.');
+          }
         });
-
-
-    }
-}
+      }
+    });
+  }
+};
