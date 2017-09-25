00de45db455cb90be253a8d621bcb6bd381830da
huazihear <huazihear@163.com>
2017-09-25 10:05:19 +0800

    update hiproxy start-up prompt

--- src/helpers/getLocalIP.js
+++ src/helpers/getLocalIP.js
@@ -9,13 +9,17 @@ module.exports = function () {
   var IPv4 = '127.0.0.1';
 
   if (process.platform === 'darwin' || process.platform === 'linux') {
-    for (var i = 0; i < os.networkInterfaces().en0.length; i++) {
-      if (os.networkInterfaces().en0[i].family === 'IPv4') {
-        IPv4 = os.networkInterfaces().en0[i].address;
-      }
+    var interfaces = os.networkInterfaces();
+
+    for (var key in interfaces) {
+      interfaces[key].forEach(function (details) {
+        if (details.family === 'IPv4' && key === 'en0' && details.address !== '127.0.0.1') {
+          IPv4 = details.address;
+        }
+      });
     }
   } else if (process.platform === 'win32') {
-    for (i = 0; i < os.networkInterfaces()['本地连接'].length; i++) {
+    for (var i = 0; i < os.networkInterfaces()['本地连接'].length; i++) {
       if (os.networkInterfaces()['本地连接'][i].family === 'IPv4') {
         IPv4 = os.networkInterfaces()['本地连接'][i].address;
       }
